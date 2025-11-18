aad3e3c966d164358994554e3775107b0e1eabc4
rinormaliqi <rmaliqi75@gmail.com>
2025-11-18 20:00:46 +0100

    asasas

--- src/components/AAA/logocomp.jsx
+++ src/components/AAA/logocomp.jsx
@@ -59,12 +59,12 @@ const LogoComp = () => {
         
         {/* Typing Animation */}
         <div className="mb-8 animate-typing overflow-hidden border-r-2 border-gray-400 whitespace-nowrap mx-auto text-xl md:text-2xl text-white">
-          Real Estate & Shit/Blerje Veturash
-        </div>
+Platform Inovative 
+       </div>
         
         {/* Description */}
         <p className="max-w-2xl mx-auto text-lg md:text-xl mb-12 text-white animate-fade-in">
-Po punojmë shumë për t'ju sjellë platformën më të mirë për blerjen dhe shitjen e pronave dhe veturave në Kosovë. Qëndroni të njoftuar për diçka të mrekullueshme!        </p>
+Po punojmë shumë për t'ju sjellë platformën më inovative në Kosovë. Qëndroni të njoftuar për diçka të mrekullueshme!        </p>
         
    
         
@@ -78,7 +78,7 @@ Po punojmë shumë për t'ju sjellë platformën më të mirë për blerjen dhe 
               value={email}
               onChange={(e) => setEmail(e.target.value)}
               required
-              className="flex-grow px-4 py-3 rounded-l-lg bg-white text-white focus:outline-none focus:ring-2 focus:ring-white"
+              className="flex-grow px-4 py-3 rounded-l-lg bg-white text-black focus:outline-none focus:ring-2 focus:ring-white"
             />
             <button 
               type="submit"
